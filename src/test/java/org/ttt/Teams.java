75f5c3e09fe90486f5c919907e2499469d087062
palanimanickam <palanimanickam8055@gmail.com>
2022-11-07 22:08:23 +0530

    MyTeam

---
+++ src/test/java/org/ttt/Teams.java
@@ -0,0 +1,13 @@
+package org.ttt;
+
+public class Teams {
+	public void india() {
+System.out.println("kohli");
+}
+	public void sa() {
+System.out.println("markram");
+}
+	public void aus() {
+System.out.println("smith");
+}
+}
